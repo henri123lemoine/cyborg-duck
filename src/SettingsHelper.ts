c1f88bb49a675b587d99b68e808236b9e7eec919
henri123lemoine <henri123lemoine@gmail.com>
2023-06-03 18:02:58 -0400

    Added a toggle for semantic search functionalities.

--- src/SettingsHelper.ts
+++ src/SettingsHelper.ts
@@ -12,6 +12,7 @@ export const DEFAULT_SETTINGS: CyborgDuckSettings = {
     openaiBaseEngineId: 'code-davinci-002',
     openaiChatEngineId: 'gpt-3.5-turbo',
     promptLibraryPath: '',
+    usePinecone: false,
 }
 
 export interface CyborgDuckSettings {
@@ -22,6 +23,7 @@ export interface CyborgDuckSettings {
     openaiBaseEngineId: 'code-davinci-002' | 'text-davinci-003' | 'text-davinci-002' | 'text-curie-001' | 'text-babbage-001' | 'text-ada-001';
     openaiChatEngineId: 'gpt-4' | 'gpt-4-0314' | 'gpt-4-32k' | 'gpt-4-32k-0314' | 'gpt-3.5-turbo' | 'gpt-3.5-turbo-0301';
     promptLibraryPath: string;
+    usePinecone: boolean;
 }
 
 export class CyborgDuckSettingTab extends PluginSettingTab {
@@ -37,7 +39,7 @@ export class CyborgDuckSettingTab extends PluginSettingTab {
 
         containerEl.empty();
 
-        containerEl.createEl('h2', { text: 'Cyborg Duck Settings' });
+        containerEl.createEl('h1', { text: 'Cyborg Duck Settings' });
         
         new Setting(containerEl)
             .setName('Prompt Library Path')
@@ -52,7 +54,7 @@ export class CyborgDuckSettingTab extends PluginSettingTab {
 
         new Setting(containerEl)
             .setName('Context Amount')
-            .setDesc('How much context to send to GPT-3')
+            .setDesc('How much context to send to the LLM?')
             .addDropdown(dropdown => 
                 dropdown
                     .addOption('whole', 'Whole note')
@@ -68,6 +70,18 @@ export class CyborgDuckSettingTab extends PluginSettingTab {
         containerEl.createEl('h3', { text: 'Pinecone' });
 
         new Setting(containerEl)
+            .setName('Use Pinecone')
+            .setDesc('Check this if you want to use prompts that use knowledge from the Alignment Research Dataset (ARD) to inform the LLM.')
+            .addToggle(toggle => toggle
+                .setValue(this.plugin.settings.usePinecone)
+                .onChange(async (value) => {
+                    this.plugin.settings.usePinecone = value;
+                    apiKeySetting.settingEl.style.display = value ? 'block' : 'none';
+                    topKSetting.settingEl.style.display = value ? 'block' : 'none';
+                    await this.plugin.saveSettings();
+                }));
+
+        const apiKeySetting = new Setting(containerEl)
             .setName('API Key')
             .setDesc('Enter your Pinecone API key')
             .addText(text => text
@@ -77,8 +91,8 @@ export class CyborgDuckSettingTab extends PluginSettingTab {
                     this.plugin.settings.pineconeApiKey = value;
                     await this.plugin.saveSettings();
                 }));
-
-        new Setting(containerEl)
+    
+        const topKSetting = new Setting(containerEl)
             .setName('Top K Results')
             .setDesc('Number of top matching results to return')
             .addText(text => text
@@ -89,6 +103,11 @@ export class CyborgDuckSettingTab extends PluginSettingTab {
                     await this.plugin.saveSettings();
                 }));
 
+        // Show/hide Pinecone settings based on the initial state
+        apiKeySetting.settingEl.style.display = this.plugin.settings.usePinecone ? 'block' : 'none';
+        topKSetting.settingEl.style.display = this.plugin.settings.usePinecone ? 'block' : 'none';
+
+    
         containerEl.createEl('h3', { text: 'OpenAI' });
         
         new Setting(containerEl)
